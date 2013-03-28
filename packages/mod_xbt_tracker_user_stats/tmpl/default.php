62452e7ea41f2c5e869ec2daafe8c493fd8f9bae
Hugo Carvalho <visigod@visigod.com>
2013-03-28 19:12:07 +0000

    removed max_session_time from mod_xbt_tracker_online_staff. We're using Joomla session time fixed some css items on mod_xbt_tracker_user_stats

--- packages/mod_xbt_tracker_user_stats/tmpl/default.php
+++ packages/mod_xbt_tracker_user_stats/tmpl/default.php
@@ -24,36 +24,6 @@ if ($user->get('guest')) {
 	return;
 }
 
-$document =& JFactory::getDocument();
-$style = '#container {
-		display: table;
-		width: 99%;
-}
-		#row  {
-		display: table-row;
-}
-		#value {
-		display: table-cell;
-		white-space: nowrap;
-}
-		#value-right {
-		display: table-cell;
-		text-align: right;
-		white-space: nowrap;
-}
-		#value-left {
-		display: table-cell;
-		text-align: left;
-		white-space: nowrap;
-}
-		#value-center {
-		display: table-cell;
-		text-align: center;
-		white-space: nowrap;
-}';
-
-$document->addStyleDeclaration( $style );
-
 ?>
 <div id="container">
 	<div>
@@ -69,6 +39,7 @@ $document->addStyleDeclaration( $style );
 				echo '<div style="display:inline-block; vertical-align:middle;">'.JText::_('MOD_TRACKER_USER_STATS_COUNTRY_TEXT').' '.$user_stats->countryName.'</div>';
 				if ($params->get('country_flag')) echo '<div style="display:inline-block; vertical-align:middle;"><img id="'.$user_stats->countryName.'" alt="'.$user_stats->countryName.'" src="'.JURI::base().$user_stats->countryImage.'" width="32px" /></div>';
 				echo '</div>';
+				echo '<div style="clear: both;"></div>';
 			}
 
 			if ($params->get('uploaded')) {
@@ -76,7 +47,8 @@ $document->addStyleDeclaration( $style );
 				echo '<div style="float: left;display:inline-block; vertical-align:middle;"><img id="'.$user_stats->uploaded.'" alt="'.$user_stats->uploaded.'" src="'.JURI::base().$params->get('uploaded_image').'" width="16px" /></div>';
 				echo '<div style="margin-left: 2px;float: left;display:inline-block; vertical-align:middle;">'.JText::_('MOD_TRACKER_USER_STATS_UPLOADED_TEXT').'</div>';
 				echo '<div style="float: right;display:inline-block; vertical-align:middle;"><span style="color:green">'.TrackerHelper::make_size($user_stats->uploaded).'</span></div>';
-				echo '</div><br />';
+				echo '</div>';
+				echo '<div style="clear: both;"></div>';
 			}
 			
 			if ($params->get('downloaded')) {
@@ -84,7 +56,8 @@ $document->addStyleDeclaration( $style );
 				echo '<div style="float: left;display:inline-block; vertical-align:middle;"><img id="'.$user_stats->downloaded.'" alt="'.$user_stats->downloaded.'" src="'.JURI::base().$params->get('downloaded_image').'" width="16px" /></div>';
 				echo '<div style="margin-left: 2px;float: left;display:inline-block; vertical-align:middle;">'.JText::_('MOD_TRACKER_USER_STATS_DOWNLOADED_TEXT').'</div>';
 				echo '<div style="float: right;display:inline-block; vertical-align:middle;"><span style="color:red">'.TrackerHelper::make_size($user_stats->downloaded).'</span></div>';
-				echo '</div><br />';
+				echo '</div>';
+				echo '<div style="clear: both;"></div>';
 			}
 
 			if ($params->get('ratio')) {
@@ -99,51 +72,67 @@ $document->addStyleDeclaration( $style );
 				else echo '<span style="color:red">';
 				echo TrackerHelper::make_ratio($user_stats->downloaded, $user_stats->uploaded);
 				echo '</span></div>';
-				echo '</div><br />';
+				echo '<div style="clear: both;"></div>';
+				echo '</div>';
 			}
 		
 
 			if ($appParams->get('enable_donations') && $params->get('donations')) {
-				echo '<div>'.JText::_('MOD_TRACKER_USER_STATS_DONATIONS_TEXT');
-				echo JText::_('MOD_TRACKER_USER_STATS_DONATIONS_CURRENCY');
+				echo '<div>';
+				echo '<div style="float: left;">'.JText::_('MOD_TRACKER_USER_STATS_DONATIONS_TEXT').'</div>';
+				echo '<div style="float: right;">'.JText::_('MOD_TRACKER_USER_STATS_DONATIONS_CURRENCY');
 				echo $user_stats->donated ? $user_stats->donated : '0';
+				echo '</div>';
+				echo '<div style="clear: both;"></div>';
 				echo '</div>';
 			}
 			
 			
 			if ($params->get('can_leech')) {
-				echo '<div>'.JText::_('MOD_TRACKER_USER_STATS_CAN_LEECH_TEXT');
-				echo $user_stats->can_leech ? JText::_('JYES') : JText::_('JNO');
+				echo '<div>';
+				echo '<div style="float: left;">'.JText::_('MOD_TRACKER_USER_STATS_CAN_LEECH_TEXT').'</div>';
+				echo '<div style="float: right;">'.$user_stats->can_leech ? JText::_('JYES') : JText::_('JNO').'</div>';
+				echo '<div style="clear: both;"></div>';
 				echo '</div>';
 			}
 
 			if ($params->get('wait_time')) {
-				echo '<div>'.JText::_('MOD_TRACKER_USER_STATS_WAIT_TIME_TEXT');
-				echo $user_stats->wait_time ? TrackerHelper::make_wait_time($user_stats->wait_time, 0) : JText::_('MOD_TRACKER_USER_STATS_WAIT_TIME_NO_WAIT_TIME'); 
+				echo '<div>';
+				echo '<div style="float: left;">'.JText::_('MOD_TRACKER_USER_STATS_WAIT_TIME_TEXT').'</div>';
+				echo '<div style="float: right;">'.$user_stats->wait_time ? TrackerHelper::make_wait_time($user_stats->wait_time, 0) : JText::_('MOD_TRACKER_USER_STATS_WAIT_TIME_NO_WAIT_TIME').'</div>'; 
+				echo '<div style="clear: both;"></div>';
 				echo '</div>';
 			}
 
 			if ($params->get('peer_limit')) {
-				echo '<div>'.JText::_('MOD_TRACKER_USER_STATS_PEER_LIMIT_TEXT');
-				echo $user_stats->peer_limit ? $user_stats->peer_limit.JText::_('MOD_TRACKER_USER_STATS_PEER_LIMIT_IPS') : JText::_('MOD_TRACKER_USER_STATS_PEER_LIMIT_UNLIMITED');
+				echo '<div>';
+				echo '<div style="float: left;">'.JText::_('MOD_TRACKER_USER_STATS_PEER_LIMIT_TEXT').'</div>';
+				echo '<div style="float: right;">'.$user_stats->peer_limit ? $user_stats->peer_limit.JText::_('MOD_TRACKER_USER_STATS_PEER_LIMIT_IPS') : JText::_('MOD_TRACKER_USER_STATS_PEER_LIMIT_UNLIMITED').'</div>';
+				echo '<div style="clear: both;"></div>';
 				echo '</div>';
 			}
 
 			if ($params->get('torrent_limit')) {
-				echo '<div>'.JText::_('MOD_TRACKER_USER_STATS_TORRENT_LIMIT_TEXT');
-				echo $user_stats->torrent_limit ? $user_stats->torrent_limit.JText::_('MOD_TRACKER_USER_STATS_TORRENT_LIMIT_TORRENTS') : JText::_('MOD_TRACKER_USER_STATS_TORRENT_LIMIT_UNLIMITED');
+				echo '<div>';
+				echo '<div style="float: left;">'.JText::_('MOD_TRACKER_USER_STATS_TORRENT_LIMIT_TEXT').'</div>';
+				echo '<div style="float: right;">'.$user_stats->torrent_limit ? $user_stats->torrent_limit.JText::_('MOD_TRACKER_USER_STATS_TORRENT_LIMIT_TORRENTS') : JText::_('MOD_TRACKER_USER_STATS_TORRENT_LIMIT_UNLIMITED').'</div>';
+				echo '<div style="clear: both;"></div>';
 				echo '</div>';
 			}
 
 			if ($params->get('upload_multiplier')) {
-				echo '<div>'.JText::_('MOD_TRACKER_USER_STATS_TORRENT_UPLOAD_MULTIPLIER_TEXT');
-				echo $user_stats->multiplier_type ? $user_stats->user_um : $user_stats->group_um;
+				echo '<div>';
+				echo '<div style="float: left;">'.JText::_('MOD_TRACKER_USER_STATS_TORRENT_UPLOAD_MULTIPLIER_TEXT').'</div>';
+				echo '<div style="float: right;">'.$user_stats->multiplier_type ? $user_stats->user_um : $user_stats->group_um.'</div>';
+				echo '<div style="clear: both;"></div>';
 				echo '</div>';
 			}
 
 			if ($params->get('download_multiplier')) {
-				echo '<div>'.JText::_('MOD_TRACKER_USER_STATS_TORRENT_DOWNLOAD_MULTIPLIER_TEXT');
-				echo $user_stats->multiplier_type ? $user_stats->user_dm : $user_stats->group_dm;
+				echo '<div>';
+				echo '<div style="float: left;">'.JText::_('MOD_TRACKER_USER_STATS_TORRENT_DOWNLOAD_MULTIPLIER_TEXT').'</div>';
+				echo '<div style="float: right;">'.$user_stats->multiplier_type ? $user_stats->user_dm : $user_stats->group_dm.'</div>';
+				echo '<div style="clear: both;"></div>';
 				echo '</div>';
 			}
 			?>
